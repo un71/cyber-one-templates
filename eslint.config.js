5b768ed5bdf9b87be8e6f33c861b5811a9cd83b6
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-02-23 19:37:03 +0500

    chore: add linters; feat: add tailwind-css

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
+  { ignores: ["build", "node_modules"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 const PORT = 4000;
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [tailwindcss()],
   server: {
     port: PORT,
     host: true,
