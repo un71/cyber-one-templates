85011c1aaaf22ba2bebd5ea1fb4252e78ca0d67c
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-02-24 12:40:45 +0500

    feat: add res-code pages, header, footer

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  eslintPluginPrettierRecommended,
-  eslintConfigPrettier,
   { ignores: ["node_modules"] },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
 import "./index.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdown = document.getElementById("lang-switcher") as HTMLElement;
+  if (!dropdown) return;
+
+  const button = document.getElementById(
+    "lang-switcher-trigger",
+  ) as HTMLButtonElement;
+  if (!button) return;
+
+  const toggleDropdown = () => {
+    const isOpen = dropdown.getAttribute("data-open") === "true";
+    dropdown.setAttribute("data-open", isOpen ? "false" : "true");
+  };
+
+  button.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleDropdown();
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!dropdown.contains(event.target as Node)) {
+      dropdown.setAttribute("data-open", "false");
+    }
+  });
+});
